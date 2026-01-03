bd4bca311e7e06c1e37afc1db232e40141286719
Emily Yao <ey7463@princeton.edu>
2026-01-02 22:16:36 -0600

    added navbar, home and about pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
 import JobsPage from "./components/JobsPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Navbar from "./components/navbar";
+
+import "./App.css";
 
 function App() {
-  return <JobsPage />
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/jobs" element={<JobsPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+
+export default App;
---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/react.svg"; // your logo file
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        backgroundColor: "#eee",
+        padding: "0.5rem 1rem",
+        display: "flex",
+        alignItems: "center",
+        zIndex: 1000,
+      }}
+    >
+      {/* Logo replaces "Home" */}
+      <Link to="/">
+        <img
+          src={logo}
+          alt="Logo"
+          style={{ height: "40px", marginRight: "1rem" }}
+        />
+      </Link>
+
+      {/* Other links */}
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <Link to="/about">About</Link>
+        <Link to="/jobs">Job Dashboard</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
